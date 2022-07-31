d4139c8235801ec353f6c04a8f331d3e5106562e
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-07-31 17:11:37 +0200

    Update sumOfNumbers.java

--- lectures/lecture1/sumOfNumbers.java
+++ lectures/lecture1/sumOfNumbers.java
@@ -3,35 +3,43 @@
   Calculate the sum of all the previously entered numbers and display it on the console.
 */
 
-
     public static void main(String[] args) {
         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-           String state;
+       // An object from Scanner to take an input to the consol with in Stream.
+       Scanner in = new Scanner(System.in);
+       
+           String value; //  Input that equals a number or word "OK" to exit.
            long sum = 0L;
            
-           lable:  do{
-               
-               try{
-                   
-                 System.out.print("Enter any number or exit by entering ok: ");
-                 state = br.readLine(); // input
-
-                // input validation
-                 switch(state){
-                     case "OK", "Ok", "oK", "ok" -> { break lable;} // case 1 to exit
-                     default -> sum += Integer.valueOf(state); //  case 2 to add the numbers entered
-                 }
-                 
-                 } catch(NumberFormatException | IOException n){
-                     // Input error
-                     System.out.println("You may have entered the wrong value");
-                 }
-                 
+           do{
+                      System.out.print("Enter any number or exit by entering ok: ");
+                      value = in.next(); // read an input from the console and store in value varibale.
+                      
+                      /*
+                      If the value of the input is equal to "OK", 
+                      the loop will be exited and the program will be terminated.
+                      */
+                      if(value.equalsIgnoreCase("OK")) break;
+                      
+                      try{
+                          
+                     /*
+                          If the value is not equal to ok, it is necessary to have bits to complete the summation process.
+                         The number will be received as a String and will be converted to Integer by the ValueOf method of the Integer class.
+                         If there is a problem with the converted number, 
+                         the method will throw an exception of NumberFormatException and skip the current iteration to add to the sum variable.
+                     */
+                      sum += Integer.valueOf(value);
+                      
+                      } catch( NumberFormatException n){
+                                System.out.println("-------> You may have entered the wrong value");
+                      }
+           
            } while(true);
            
-                // the result
-               System.out.println("Finished....");
-               System.out.println("sum: " + sum);
+           // Final result
+           System.out.println("#################################");
+           System.out.println("Finished......");
+           System.out.println("Sum: " + sum);
+           
            }
