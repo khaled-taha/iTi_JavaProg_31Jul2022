8ecd2182820ea90a858543ddc57c9f1c82ddf36c
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-07-31 14:12:51 +0200

    Update MaxTwoNumbers.java

--- lectures/lecture1/MaxTwoNumbers.java
+++ lectures/lecture1/MaxTwoNumbers.java
@@ -3,6 +3,7 @@ This is a program to take two numbers from the keyboard and print the max of the
 */    
 
     public static void main(String[] args) {
+        
         Scanner in = new Scanner(System.in);
         // Object from Scanner to take an input to the consol with keyboard Stream
         int num1 , num2, max;
@@ -10,9 +11,7 @@ This is a program to take two numbers from the keyboard and print the max of the
         try{
                num1 = in.nextInt(); // first Input
                num2 = in.nextInt(); // second Input
-               
-               max = (num1 > num2) ? num1 : num2; // the result will be stored in max variable
-              System.out.println("Max: " + max);
+              System.out.println("Max: " + Math.max(num1, num2));
               
         }catch(InputMismatchException | IllegalStateException m) {
             System.out.println("Error......!");
