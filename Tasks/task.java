71fd2a9d191fc259416b31bc4d2259898aff0eef
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-07-31 19:50:13 +0200

    Update task.java

--- Tasks/task.java
+++ Tasks/task.java
@@ -4,3 +4,41 @@ Write a program and ask the user to enter a few words separated by a space. Use 
 For example, if the user types: "number of students", display "NumberOfStudents". 
 Make sure that the program is not dependent on the input. So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
 */
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        StringBuffer sb = new StringBuffer(in.nextLine());
+        int StringLength = sb.length();
+        boolean change = true;
+        char ch;
+        int i = 0;
+        
+        while ( i < StringLength) {
+            ch = sb.charAt(i);
+            
+            if(!change && (ch >= 65 && ch <= 90)){
+                ch += 32;
+                sb.setCharAt(i, ch);
+            }
+            
+           else if(change && (ch >= 97 && ch <= 122)){
+                ch -= 32;
+                sb.setCharAt(i, ch);
+            }
+            
+            else if (ch == ' '){
+                sb.deleteCharAt(i);
+                StringLength--;
+                change = true;
+                continue;
+            }
+            change = false;
+            i++;
+            
+        }
+        
+        System.out.println(sb);
+        
+           }
