0f5a3d53f60e571daadc181010b37f7245f1740f
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-08-02 12:02:16 +0200

    Update quiz1.java

--- lectures/lectrue 3/quiz1.java
+++ lectures/lectrue 3/quiz1.java
@@ -1,4 +1,12 @@
 
+/*
+design a class called stopwatch. the job of this class is to simulate a stopwatch. it should provide two methods: start and stop.
+we call the start method first, and the stop method next. then we ask the stopwatch about the duration between start and stop.
+duration should be a value in timespan. display the duration on the console. we should also be able to use a stopwatch multiple times.
+so we may start and stop it and then start and stop it again. make sure the duration value each time is calculated properly.
+we should not be able to start a stopwatch twice in a row (because that may overwrite the initial start time).
+so the class should throw an error if its started twice
+*/
 
 import java.util.Calendar;
 
