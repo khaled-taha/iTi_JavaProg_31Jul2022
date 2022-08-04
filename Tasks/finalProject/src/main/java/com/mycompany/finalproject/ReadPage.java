656b57c83ca1a5d2a136d6403605968a0b22c3c9
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-08-04 14:32:36 +0200

    Update ReadPage.java

--- Tasks/finalProject/src/main/java/com/mycompany/finalproject/ReadPage.java
+++ Tasks/finalProject/src/main/java/com/mycompany/finalproject/ReadPage.java
@@ -13,7 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class ReadPage extends Thread implements Runnable {
+public class ReadPage extends Thread {
 
     private File f;
     private String urlString;
