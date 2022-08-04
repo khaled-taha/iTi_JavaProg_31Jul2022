217e3065b1dbcffe7f23938bf2a6c83b9ec38989
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-08-04 15:35:49 +0200

    Update ReadPage.java

--- Tasks/finalProject/src/main/java/com/mycompany/finalproject/ReadPage.java
+++ Tasks/finalProject/src/main/java/com/mycompany/finalproject/ReadPage.java
@@ -1,16 +1,12 @@
 package com.mycompany.finalproject;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class ReadPage extends Thread {
